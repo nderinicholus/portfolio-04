16001752224985e4b93012074ae5dd8fca47fd3d
Nick Nderi <nicknderi@Nicks-MacBook-Pro.local>
2021-08-04 13:08:30 +0300

    Layout extends

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -26,6 +27,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -33,6 +35,39 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/about">About</a>
+                          </li>
+
+                          <li class="nav-item">
+                            <a class="nav-link" href="/services">Services</a>
+                          </li>
+
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Service B
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Lorem ipsum dolor sit amet.</a>
+
+                              <a class="dropdown-item" href="#">Lorem ipsum dolor sit amet consectetur.</a>
+
+                              <a class="dropdown-item" href="#">Lorem ipsum dolor sit amet consectetur adipisicing.</a>
+                              
+                            </div>
+                          </li>
+
+                          <li class="nav-item">
+                            <a class="nav-link" href="/about">Portfolio</a>
+                          </li>
+
+                          <li class="nav-item">
+                            <a class="nav-link" href="/contact">Contact</a>
+                          </li>
 
                     </ul>
 
